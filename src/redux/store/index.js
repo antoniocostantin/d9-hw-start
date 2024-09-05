49032f420f686e6b230534f958dd121ed64f465b
antoniocostantin <costantini062@gmail.com>
2024-09-05 20:59:57 +0200

    fine

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,5 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
-import mainReducer from '../reducers'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import favouriteReducer from '../reducers/favourite'
+import jobReducer from '../reducers/job'
+
+const mainReducer = combineReducers({
+  favourite: favouriteReducer,
+  job: jobReducer,
+})
 
 const store = configureStore({
     reducer: mainReducer, // qui ci va il reducer
